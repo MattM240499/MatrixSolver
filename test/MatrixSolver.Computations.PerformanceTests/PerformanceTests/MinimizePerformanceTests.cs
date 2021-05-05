824dc144aa2b3af367507207f14f720ad2269785
MattM240499 <mattmillward2404@gmail.com>
2021-05-05 04:13:11 +0100

    Report writing changes

--- test/MatrixSolver.Computations.PerformanceTests/PerformanceTests/MinimizePerformanceTests.cs
+++ test/MatrixSolver.Computations.PerformanceTests/PerformanceTests/MinimizePerformanceTests.cs
@@ -15,7 +15,7 @@ namespace MatrixSolver.Computations.PerformanceTests
             _output = output;
         }
 
-        [Fact]
+        [Fact(Skip = "Performance test only")]
         public void Minimize_PerformanceTests()
         {
             var automaton = PerformanceTestingUtility.CreateLargeAutomataWithRandomTransitions(_alphabet, 10000);
